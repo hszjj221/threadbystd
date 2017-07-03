ec4b2a92c8b4c213e9c4db64cc064c3d695bab3d
hszjj <hszjj221@163.com>
2017-07-03 22:31:23 +0800

    添加基本文件

--- thread_pool.h
+++ thread_pool.h
@@ -13,5 +13,49 @@
 #include <iostream>
 #include <type_traits>
 
+#include "thread_guard.h"
+
+namespace tp {
+
+class Thread_pool {
+private:
+	typedef std::function<void()> task_type;
+private:
+	std::atomic<bool> stop_;
+	std::mutex mtx_;
+	std::condition_variable cond_;
+	std::queue<task_type> tasks_;
+	std::vector<std::thread> threads_;
+	tp::Thread_guard<std::thread> tg_;
+public:
+	Thread_pool();
+};
+
+Thread_pool::Thread_pool()
+	:stop_(false)
+	, tg_(threads_) {
+	size_t nthreads = std::thread::hardware_concurrency();
+	nthreads = nthreads == 0 ? 2 : nthreads;
+
+	for (size_t i = 0; i != nthreads; ++i) {
+		threads_.push_back(std::thread(
+			[this] {
+			while (!stop_) {
+				task_type task;
+				{
+					std::unique_lock<std::mutex> lock(this->mtx_);
+					this->cond_.wait(lock, [this] {return !this->tasks_.empty(); });
+					task = std::move(this->tasks_.front());
+					this->tasks_.pop();
+				}
+				task();
+			}
+		}
+		));
+	}
+}
+
+}
+
 #endif // !_THREAD_POOL_H_
 
